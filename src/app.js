8a816f2170c3a2f941fa1854931af5022b794ce9
perfavore8 <nekit2353@mail.ru>
2023-03-28 14:26:23 +0200

    some refactoring

--- src/app.js
+++ src/app.js
@@ -1,64 +1,48 @@
-import Vue from 'vue/dist/vue.js';
-import Settings from './components/settings.vue';
-import store from './stores/index';
+import Vue from "vue/dist/vue.js";
+import Settings from "./components/settings.vue";
+import store from "./stores/index";
 
 Vue.config.devtools = true;
 Vue.config.silent = true;
 
 const Widget = {
+  render(self, func) {
+    console.log("123", self, func);
 
-    render() {
+    return true;
+  },
 
+  init() {
+    console.debug("oneDocs init");
+    return true;
+  },
 
-        return true;
-    },
+  bind_actions() {
+    console.debug("oneDocs bind_actions");
+    return true;
+  },
 
-    init() {
-        console.debug('oneDocs init');
-        return true;
-    },
+  settings() {
+    window.vue = new Vue({
+      store: store,
+      render: (h) => h(Settings),
+    }).$mount(Settings.$el);
 
-    bind_actions() {
+    // After you create app
+    window.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = window.vue.constructor;
 
-        console.debug('oneDocs bind_actions');
-        return true;
-    },
+    console.debug("oneDocs settings");
+  },
 
-    settings() {
+  onSave() {},
 
-        window.vue = new Vue({
-            store: store,
-            render: h => h(Settings),
-        }).$mount(Settings.$el);
+  destroy() {},
 
-        // After you create app
-        window.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = window.vue.constructor;
+  contacts_selected() {},
 
-        console.debug('oneDocs settings');
-    },
+  leads_selected() {},
 
-    onSave() {
-
-    },
-
-    destroy() {
-
-    },
-
-    contacts_selected() {
-
-    },
-
-    leads_selected() {
-
-    },
-
-    tasks_selected() {
-
-    }
-
+  tasks_selected() {},
 };
-
-
 
 export default Widget;
