3803aab54d921a78f65c0a1faf1cb364aa9fc507
perfavore8 <nekit2353@mail.ru>
2023-03-29 10:37:35 +0200

    some refactoring

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,12 +1,41 @@
 <template>
-  <div>123</div>
+  <div class="container">
+    <div class="header">header</div>
+    <div class="content">content</div>
+    <div class="footer">footer</div>
+  </div>
 </template>
 
 <script>
 export default {
   components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  mounted() {},
+  watch: {},
+  methods: {},
 };
 </script>
 
 <style scoped>
+/* .app {
+} */
+.container {
+  overflow-y: auto;
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+}
+.header {
+  margin-bottom: 30px;
+}
+.content::-webkit-scrollbar {
+  width: 0;
+}
+.footer {
+  display: flex;
+  justify-content: end;
+}
 </style>
